719b5e54bf0927f34d1991fd8f81862466d4b871
areeb mansoori <areebmansoori92@gmail.com>
2024-09-12 19:50:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import Card from '../components/Card'
+
+const App = () => {
+
+  const [data, setData] = useState(null)
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/')
+    .then(res=>res.json())
+    .then((res)=>{
+      console.log(res)
+      setData(res)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  },[])
+  return (
+    <>
+    <h1 className='text-center text-3xl mt-7'> The Hub </h1>
+    {
+      data ? data.map((item,index)=>{
+   return(
+    <div className='flex justify-center gap-5' key={index}>
+           <Card src={item.image} title= {item.category} price={item.price} description={item.description}/>
+    </div> 
+   )
+      }):<div class="flex items-center justify-center min-h-screen">
+      <div>
+        <span class="loading loading-bars loading-xs"></span>
+        <span class="loading loading-bars loading-sm"></span>
+        <span class="loading loading-bars loading-md"></span>
+        <span class="loading loading-bars loading-lg"></span>
+      </div>
+    </div>
+    }
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+
+
+function Card({image, title, price, desc, items}) {
+  
+  const navigate = useNavigate()
+  // Use props directly instead of `items`
+  const singleUser = (items) => {
+    console.log(items); // Logs the entire props object
+    navigate(`Singleuser/${items.id}`);
+  }
+
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img 
+          style={{
+            width: "30rem",
+            height: "17rem",
+          }}
+          src={image}
+          alt="Shoes"
+          className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{title}</h2>
+        <p>Price: {price}</p>
+        <h4>{desc}</h4>
+        <div className="card-actions">
+          <button onClick={()=>singleUser(items)} className="btn btn-primary">Show More</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
